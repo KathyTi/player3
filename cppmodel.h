9b650eda70acf85fd15cf7424811157edf2a1dd6
Petr Mironychev <artifeks92@gmail.com>
2020-04-17 19:46:47 +0300

    Update model

--- cppmodel.h
+++ cppmodel.h
@@ -1,45 +1,58 @@
 #ifndef CPPMODEL_H
 #define CPPMODEL_H
-#include <QString>
-#include <QModelIndex>
 #include <QAbstractListModel>
+#include <QModelIndex>
+#include <QString>
 
-class CppModel : public QAbstractListModel
-{
-    Q_OBJECT
+class CppModel : public QAbstractListModel {
+  Q_OBJECT
 public:
-    explicit CppModel(QAbstractListModel *parent = nullptr);
-    enum Roles{
-        nameRole = Qt::UserRole+1,
-        iconRole,
-        numberRole,
-        colorRole,
-        pathRole,
-        markRole
-    };
-    struct elementData
-    {
-        QString m_name;
-        QString m_icon;
-        QString m_number;
-        QString m_color;
-        QString m_path;
-        QString m_mark;
-        elementData(const QString& name, const QString& icon, const QString& number, const QString& color, const QString& path, const QString& mark)
-            :m_name(name), m_icon(icon), m_number(number), m_color(color), m_path(path), m_mark(mark){}
-    };
-    CppModel();
+  explicit CppModel();
+  enum Roles {
+    nameRole = Qt::UserRole + 1,
+    iconRole,
+    numberRole,
+    colorRole,
+    pathRole,
+    markRole,
+    mark_numberRole,
+    mark_visibleRole
+  };
+  struct ElementData {
+    QString m_name;
+    QString m_icon;
+    QString m_number;
+    QString m_color;
+    QString m_path;
+    QString m_mark;
+    QString m_mark_number;
+    QString m_mark_visible;
+    ElementData(const QString &name, const QString &icon, const QString &number,
+                const QString &color, const QString &path, const QString &mark,
+                const QString &mark_number, const QString &mark_visible)
+        : m_name(name), m_icon(icon), m_number(number), m_color(color),
+          m_path(path), m_mark(mark), m_mark_number(mark_number),
+          m_mark_visible(mark_visible) {}
+  };
 public slots:
-    //rowCount, data, roles и в твоем случае canFetchMore и FetchMore.
-    virtual int rowCount(const QModelIndex &parent) const override;
-    virtual QVariant data(const QModelIndex &index, int role) const override;
-    virtual QHash<int, QByteArray> roleNames() const override;
-    void addElement(const QString &name, const QString &icon, const QString &number, const QString &color, const QString &path, const QString &mark);
-    void appendElement(const QString& name, const QString& icon, const QString& number, const QString& color, const QString& path, const QString& mark);
-    void clearModel();
+  // rowCount, data, roles и в твоем случае canFetchMore и FetchMore.
+  int rowCount(const QModelIndex &parent) const override;
+  QVariant data(const QModelIndex &index, int role) const override;
+  QHash<int, QByteArray> roleNames() const override;
+  void addElement(const QString &name, const QString &icon,
+                  const QString &number, const QString &color,
+                  const QString &path, const QString &mark,
+                  const QString &mark_number, const QString &mark_visible);
+  void appendElement(const QString &name, const QString &icon,
+                     const QString &number, const QString &color,
+                     const QString &path, const QString &mark,
+                     const QString &mark_number, const QString &mark_visible);
+  void clearModel();
+  void setElementProperty(const QModelIndex &index, int role, QString data);
+
 private:
-    QString tmpstr;
-    QList <elementData> dataList;
+  QString tmpstr;
+  QList<ElementData> dataList;
 };
 
 #endif // CPPMODEL_H
